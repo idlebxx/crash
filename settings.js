1b6750495a165184e9d358d6835dafb7b49636fa
idlebxx <idlebxx3@gmail.com>
2026-06-03 00:57:10 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,59 +1,54 @@
 const fs = require("fs");
+const chalk = require("chalk");
 
-const chalk = require("chalk")
+// ⭐⭐⭐ ضع التوكن الخاص بك هنا ⭐⭐⭐
+global.BOT_TOKEN = "8788758313:AAHNtqL_gXLslINX7OW9o6VYuhCnOmbgH0Q"; 
 
-//global.BOT_TOKEN = "" // create bot here https://t.me/Botfather and get bot token
+global.BOT_NAME = "𝗖𝗥𝗔𝗦𝗛 𝗜𝗗𝗟𝗘𝗕 𝗫";
+global.OWNER_NAME = "https://t.me/idlebx2";
+global.OWNER = ["https://t.me/idlebx2", "https://www.youtube.com/@abnadlep"];
 
-global.BOT_NAME = "𝗖𝗥𝗔𝗦𝗛 𝗜𝗗𝗟𝗘𝗕 𝗫" //your bot name
+// ⭐⭐⭐ تأكد من صحة هذه المعرفات ⭐⭐⭐
+global.DEVELOPER = ["7240148750"];
 
-global.OWNER_NAME = "https://t.me/idlebx2" //your name with sign @
+global.pp = 'https://i.postimg.cc/gj3Pqv70/idleb.jpg';
+global.link = 'https://whatsapp.com/channel/0029Vb8035sJ93wZlpYTP10z';
+global.idch = 'https://whatsapp.com/channel/0029Vb8035sJ93wZlpYTP10z';
 
-global.OWNER = ["https://t.me/idlebx2", "https://www.youtube.com/@abnadlep"] // Make sure the username is correct so that the special owner features can be used.
+// إعدادات التليجرام
+global.GROUP_ID = -1003235079144; 
+global.CHANNEL_ID =  -1003992890677; 
+global.GROUP_LINK = "https://t.me/idlebx2";
+global.CHANNEL_INVITE_LINK = "https://t.me/idlebx2";
+global.WHATSAPP_LINK = "https://whatsapp.com/channel/0029Vb8035sJ93wZlpYTP10z";
+global.YOUTUBE_LINK = "https://www.youtube.com/@idlebx2";
+global.INSTAGRAM_LINK = "https://instagram.com/xlb_me";
 
-global.DEVELOPER = ["7240148750"] //developer telegram id to operate addprem delprem and listprem
+global.baileysDB = 'baileysDB.json';
+global.botDb = 'database/database.json';
 
-global.ppp = 'https://streamable.com/ddswro' //your bot pp
+global.prefa = ['','!','.',',','🐤','🗿'];
 
-global.pp = 'https://i.postimg.cc/gj3Pqv70/idleb.jpg'
+global.mess = {
+    success: 'Done✓',
+    admin: 'This Command Can Only Be Used By Group Admins !',
+    botAdmin: 'This Command Can Only Be Used When Bot Becomes Group Admin !',
+    OnlyOwner: 'This Command Can Only Be Used By Premium User !',
+    OnlyGrup: 'This Command Can Only Be Used In Group Chat !',
+    private: 'This Command Can Only Be Used In Private Chat !',
+    wait: 'Wait a minute...',
+    notregist: 'You are not registered in the Bot Database.',
+    premium: 'This Command Can Only Be Used By Premium User !'
+}
 
-//approval
-
-global.GROUP_ID = -1003235079144; // Replace with your group ID
-
-global.CHANNEL_ID =  -1003992890677; // Replace with your channel ID
-
-global.GROUP_LINK = "https://t.me/idlebx2"; // Replace with your group link
-
-global.CHANNEL_INVITE_LINK = "https://t.me/idlebx2"; // Replace with your private channel invite link
-
-    global.WHATSAPP_LINK = "https://whatsapp.com/channel/0029Vb8035sJ93wZlpYTP10z"; // Replace with your group link
-
-global.YOUTUBE_LINK = "https://www.youtube.com/@idlebx2"; // Replace with your youtube link
-
-global.INSTAGRAM_LINK = "https://instagram.com/xlb_me"; // Replace with your ig link
-
-global.owner = global.owner = ['963969061988'] //owner whatsapp
-
-const {
-
-   english
-
-} = require("./lib");
-
-global.language = english
-
-global.lang = language
+const { english } = require("./lib/lang");
+global.language = english;
+global.lang = language;
 
 let file = require.resolve(__filename)
-
 fs.watchFile(file, () => {
-
-fs.unwatchFile(file)
-
-console.log(chalk.redBright(`Update ${__filename}`))
-
-delete require.cache[file]
-
-require(file)
-
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update ${__filename}`))
+    delete require.cache[file]
+    require(file)
 })
